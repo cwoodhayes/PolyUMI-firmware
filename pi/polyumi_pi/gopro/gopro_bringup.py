778a45f42bb586bdd1169a765093ed6505380bdd
conorbot <cwoodhayes@gmail.com>
2026-03-19 21:25:36 -0500

    connecting to gopro successfully

--- pi/polyumi_pi/gopro/gopro_bringup.py
+++ pi/polyumi_pi/gopro/gopro_bringup.py
@@ -1,137 +1,102 @@
-# video.py/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro).
-# This copyright was auto-generated on Wed, Sep  1, 2021  5:05:46 PM
+"""BLE bring-up test for connecting to a GoPro by name suffix."""
 
-"""Entrypoint for taking a video demo."""
+from __future__ import annotations
 
 import argparse
+import asyncio
 import logging
-import time
-from pathlib import Path
-from typing import Optional, Tuple
+import sys
+from typing import Optional
 
-from open_gopro import GoPro, Params
+from bleak import BleakScanner
+from bleak.backends.device import BLEDevice
+from open_gopro import GoPro
 from open_gopro.util import setup_logging
-from rich.console import Console
 
 logger = logging.getLogger(__name__)
-console = Console()  # rich consoler printer
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse command-line args."""
+    parser = argparse.ArgumentParser(description='Connect to GoPro over BLE.')
+    parser.add_argument(
+        '--name-suffix',
+        type=str,
+        default='1112',
+        help='Target BLE device name suffix (default: 1112).',
+    )
+    parser.add_argument(
+        '--scan-timeout',
+        type=float,
+        default=6.0,
+        help='BLE scan timeout in seconds for each attempt.',
+    )
+    parser.add_argument(
+        '--scan-retries',
+        type=int,
+        default=4,
+        help='Number of BLE scan attempts before giving up.',
+    )
+    return parser.parse_args()
+
+
+def discover_device(
+    name_suffix: str, timeout: float, retries: int
+) -> Optional[BLEDevice]:
+    """Discover BLE devices and return one whose name ends with name_suffix."""
+    target_suffix = name_suffix.upper()
+    for attempt in range(1, retries + 1):
+        logger.info('BLE scan attempt %s/%s (%.1fs)', attempt, retries, timeout)
+        devices = asyncio.run(BleakScanner.discover(timeout=timeout))
+
+        visible = [d for d in devices if d.name]
+        if visible:
+            logger.info('Visible BLE devices:')
+            for d in visible:
+                logger.info('  - %s (%s)', d.name, d.address)
+        else:
+            logger.info('No named BLE devices found in this scan.')
+
+        for d in visible:
+            if d.name.upper().endswith(target_suffix):
+                logger.info('Selected target device: %s (%s)', d.name, d.address)
+                return d
+
+    return None
 
 
 def main() -> int:
-    """
-    Main program functionality
-
-    Returns:
-        int: program return code
-
-    """
-    identifier, log_location, output_location, record_time, wifi_interface = (
-        parse_arguments()
-    )
+    """Discover and connect to GoPro over BLE."""
+    args = parse_args()
 
     global logger
-    logger = setup_logging(logger, log_location)
+    logger = setup_logging(logger)
 
-    gopro: Optional[GoPro] = None
-    return_code = 0
     try:
-        with GoPro(identifier, wifi_interface=wifi_interface) as gopro:
-            logger.info('CONNECTED ETC')
+        device = discover_device(
+            name_suffix=args.name_suffix,
+            timeout=args.scan_timeout,
+            retries=args.scan_retries,
+        )
+        if device is None:
+            logger.error(
+                'Could not find a BLE device ending with suffix "%s".',
+                args.name_suffix,
+            )
+            return 1
+
+        # TURNS OUT. the gopro renaming used in the app doesn't change the BLE Name, so
+        # we can just look for the default name ie "GoPro XXXX"
+        with GoPro(device, enable_wifi=False) as gopro:
+            logger.info('CONNECTED over BLE to GoPro: %s', gopro.identifier)
             return 0
-            # Configure settings to prepare for video
-            if gopro.is_encoding:
-                assert gopro.ble_command.set_shutter(Params.Shutter.OFF).is_ok
-            assert gopro.ble_setting.video_performance_mode.set(
-                Params.PerformanceMode.MAX_PERFORMANCE
-            ).is_ok
-            assert gopro.ble_setting.max_lens_mode.set(Params.MaxLensMode.DEFAULT).is_ok
-            assert gopro.ble_command.set_turbo_mode(False).is_ok
-            assert gopro.ble_command.load_preset(Params.Preset.CINEMATIC).is_ok
-
-            # Get the media list before
-            media_set_before = set(
-                x['n'] for x in gopro.wifi_command.get_media_list().flatten
-            )
-            # Take a video
-            console.print('Capturing a video...')
-            assert gopro.ble_command.set_shutter(Params.Shutter.ON).is_ok
-            time.sleep(record_time)
-            assert gopro.ble_command.set_shutter(Params.Shutter.OFF).is_ok
-
-            # Get the media list after
-            media_set_after = set(
-                x['n'] for x in gopro.wifi_command.get_media_list().flatten
-            )
-            # The video (is most likely) the difference between the two sets
-            video = media_set_after.difference(media_set_before).pop()
-            # Download the video
-            console.print('Downloading the video...')
-            gopro.wifi_command.download_file(
-                camera_file=video, local_file=output_location
-            )
-            console.print(
-                f'Success!! :smiley: File has been downloaded to {output_location}'
-            )
-
     except KeyboardInterrupt:
-        logger.warning('Received keyboard interrupt. Shutting down...')
-    if gopro is not None:
-        gopro.close()
-    console.print('Exiting...')
-    return return_code
-
-
-def parse_arguments() -> Tuple[Optional[str], Path, Path, float, Optional[str]]:
-    """
-    Parse command line arguments
-
-    Returns:
-        Tuple[str, Path, Path, float, Optional[str]]: (identifier, path to save log, path to store video,
-            record_time, wifi interface)
-
-    """
-    parser = argparse.ArgumentParser(
-        description='Connect to a GoPro camera, take a video, then download it.'
-    )
-    parser.add_argument(
-        'record_time',
-        type=float,
-        help='How long to record for',
-    )
-    parser.add_argument(
-        '-i',
-        '--identifier',
-        type=str,
-        help='Last 4 digits of GoPro serial number, which is the last 4 digits of the default camera SSID. If \
-            not used, first discovered GoPro will be connected to',
-        default=None,
-    )
-    parser.add_argument(
-        '-l',
-        '--log',
-        type=Path,
-        help='Location to store detailed log',
-        default=Path('video.log'),
-    )
-    parser.add_argument(
-        '-o',
-        '--output',
-        type=Path,
-        help="Where to write the video to. If not set, write to 'video.mp4'",
-        default=Path('video.mp4'),
-    )
-    parser.add_argument(
-        '-w',
-        '--wifi_interface',
-        type=str,
-        help='System Wifi Interface. If not set, first discovered interface will be used.',
-        default=None,
-    )
-    args = parser.parse_args()
-
-    return args.identifier, args.log, args.output, args.record_time, args.wifi_interface
+        logger.warning('Interrupted by user.')
+        return 130
+    except Exception as exc:  # pragma: no cover - hardware-dependent
+        logger.exception('BLE bringup failed: %s', exc)
+        return 1
 
 
 if __name__ == '__main__':
-    # main()
-    main()
+    sys.exit(main())
